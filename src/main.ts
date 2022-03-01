fab11765fe4f5f37a941efeac82674811368aa5e
nd <andy.spizhavka@belkins.io>
2022-03-01 13:25:15 +0200

    init store

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue';
 import { createSSRApp } from 'vue';
 import { createRouter } from './router';
 import { createHead } from '@vueuse/head';
+import { createPinia } from 'pinia';
 
 import VueLazyLoad from 'vue3-lazyload';
 
@@ -11,7 +12,7 @@ import 'virtual:svg-icons-register';
 
 import '@/assets/scss/index.scss';
 
-export function createApp() {
+export function createApp(state?: any) {
   const app = createSSRApp(App);
 
   const router = createRouter();
@@ -20,7 +21,11 @@ export function createApp() {
   app.use(router);
   app.use(head);
 
+  const store = createPinia();
+  if (state) store.state.value = state;
+
   app.use(VueLazyLoad);
+  app.use(store);
 
   app.component('SvgIcon', SvgIcon);
 
